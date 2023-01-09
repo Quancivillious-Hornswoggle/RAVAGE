ac1038d68aab419641da773e414005f6fd5c12f7
Meta <89297040+Meta-bhsc@users.noreply.github.com>
2023-01-09 00:32:52 -0800

    Add files via upload

---
+++ Email_Spammer.py
@@ -0,0 +1,84 @@
+import time
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+#--------------------------------------------------------------------------------
+def clear_console():
+    os.system('clear') 
+#--------------------------------------------------------------------------------
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp.ehlo()
+smtp.starttls()
+#--------------------------------------------------------------------------------
+def Close():
+    Exit = input('Type Exit  to close  :  ') #create input check to exit code
+    
+    if ((Exit == 'Exit') or (Exit == 'exit')):
+        quit()
+    else:
+        clear_console()
+        Close()
+#--------------------------------------------------------------------------------
+def start(pe, password):
+
+    smtp.login(pe, password)
+    number = 0
+    
+    detect = input('Start Program? Yes/No  :  ') #Create input check to start code
+
+    if ((detect == 'Yes') or (detect == 'yes')):
+        email = input("Who's Email Is Being Spammed  :  ") #Create input check to start code
+        loopAmmount = input('How Many Times Should This Loop?  :  ') #Create input check to start code
+        print(f'This Will Loop {loopAmmount} Times')
+        delayInput = input('How Many Seconds Of Delay Should There Be Between Each Email?  :  ')
+        time.sleep(1)
+        print(f'There Will Be {delayInput} Seconds Of Delay Between Each Email')
+        print('Starting In 3 Seconds...')
+        time.sleep(1)
+        print('Starting In 2 Seconds...')
+        time.sleep(1)
+        print('Starting In 1 Seconds...')
+        time.sleep(1)
+        print('Starting...')
+        converted_num = int(loopAmmount)
+        delay = int(delayInput)
+
+        time.sleep(2)
+
+        while converted_num >= 1:
+#-----------------------------------------------------------------------------------------Program Being Looped V----
+            number = number + 1
+
+            text = (f'{number}')
+            subject = 'blank'
+
+            msg = MIMEMultipart()
+            msg['Subject'] = subject
+            msg.attach(MIMEText(text))
+
+            to = [f"{email}"]
+
+            smtp.sendmail(from_addr=pe,
+              to_addrs=to, msg=msg.as_string())
+
+            print(f'Email Number {number} Is Being Sent')
+            
+            time.sleep(delay)
+#-------------------------------------------------------------------------------------------------------------------------------------
+            converted_num = converted_num - 1
+            if converted_num == 0:
+                clear_console()
+                Close()
+                break
+#-------------------------------------------------------------------------------^---------------------------------------------------
+    elif ((detect == 'No') or (detect == 'no')):
+        quit()
+
+    else:
+        clear_console() #Clear Console
+        print('Please Choose Option')
+        start()
+#--------------------------------------------------------------------------------
