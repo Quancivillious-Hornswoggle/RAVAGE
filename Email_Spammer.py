900b20b4df29241b52045f85920695f6e63ebd97
Meta <89297040+Meta-bhsc@users.noreply.github.com>
2023-01-25 20:25:27 -0800

    Newtwork Scanner

--- Email_Spammer.py
+++ Email_Spammer.py
@@ -1,93 +1,120 @@
-import time
 import os
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
+from scapy.all import ARP, Ether, srp
 import colorama
 from colorama import Fore
-#--------------------------------------------------------------------------------
-def clear_console():
-    os.system('clear') 
-#--------------------------------------------------------------------------------
- 
-#--------------------------------------------------------------------------------
-  
-#--------------------------------------------------------------------------------
-def Run(pe, password):
-    smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp.ehlo()
-    smtp.starttls()
-    print(Fore.BLUE+"██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████")
-    print("█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███")
-    print("█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███")
-    print("█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███")
-    print("█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░████░░▄▀░░███")
-    print("█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███")
-    print("█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███")
-    print("█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░░░░░██░░▄▀░░█░░▄▀░░██░░░░░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░░░███")
-    print("█████████░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█████")
-    print("█░░░░░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░░░░░█")
-    print("█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█")
-    print("█░░░░░░░░░░░░░░█░░░░░░█████████░░░░░░██░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█")
-    print("██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████")
 
-    smtp.login(pe, password)
-    number = 0
-    
-    detect = input(Fore.WHITE+'Start Program? Yes/No >>> ') #Create input check to start code
+addresses = []
 
-    if ((detect == 'Yes') or (detect == 'yes')):
-        email = input("Who's Email Is Being Spammed >>>") #Create input check to start code
-        loopAmmount = input('How Many Times Should This Loop? >>> ') #Create input check to start code
-        print(f'This Will Loop {loopAmmount} Times')
-        delayInput = input('How Many Seconds Of Delay Should There Be Between Each Email? >>> ')
-        time.sleep(1)
-        print(f'There Will Be {delayInput} Seconds Of Delay Between Each Email')
-        print('Starting In 3 Seconds...')
-        time.sleep(1)
-        print('Starting In 2 Seconds...')
-        time.sleep(1)
-        print('Starting In 1 Seconds...')
-        time.sleep(1)
-        print('Starting...')
-        converted_num = int(loopAmmount)
-        delay = int(delayInput)
+def scan(ip_range):		
+	arp = ARP(pdst=ip_range)
+	ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+	packet = ether/arp
 
-        time.sleep(2)
+	result = srp(packet, timeout=3, verbose=0)[0]
+	
+	clients = []
+	for sent, received in result:
+		clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+	return clients
+	
+def Fullscan(subnet, time):		
+	for i in range(1, 255):
+		ip_range = (subnet + '.' + str(i) + '.0/24')
+		print('Scanning Hosts on: ' + ip_range)
+		arp = ARP(pdst=ip_range)
+		ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+		packet = ether/arp
 
-        while converted_num >= 1:
-#-----------------------------------------------------------------------------------------Program Being Looped V----
-            number = number + 1
+# Send the packet on the network and receive the response
+		result = srp(packet, timeout=time, verbose=0)[0]
 
-            text = (f'{number}')
-            subject = ''
+# Print the host IPs that responded
+		for sent, received in result:
+			addresses.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-            msg = MIMEMultipart()
-            msg['Subject'] = subject
-            msg.attach(MIMEText(text))
+	for client in addresses:
+		print(f"IP: {client['ip']}   MAC: {client['mac']}")
 
-            to = [f"{email}"]
+	
+def SpecificScan():
+	os.system('clear')
+	baseip = input('Type in the first 7 numbers of the ip. EX 192.168.1  >>> ')
+	print('Scanning...')		
+	print('')
+	clients = scan(baseip + '.1/24')
+	
+	for client in clients:
+		print("IP: " + client["ip"])
+		print("MAC: " + client["mac"])
+		print("")
 
-            smtp.sendmail(from_addr=pe,
-              to_addrs=to, msg=msg.as_string())
+	choice = input('Would you like to save these to a file? y/n >>> ')
+	if(choice == 'y'):
+		print('Saving to file')
+		with open("IP_SAVED_DATA.txt", "a") as file:
+			for client in adresses:
+				file.write("IP: " + client["ip"])
+				file.write("   MAC: " + client["mac"] + '\n')
+				file.write("")
+		print('')
+		print('Done!')
+		print('')
+		input(Fore.RED+"Press Enter to continue...")
+		os.system('sudo python RAVAGE.py')
+	if(choice == 'n'):
+		input(Fore.RED+"Press Enter to continue...")
+		os.system('sudo python RAVAGE.py')
+		
+def FullScan():
+	os.system('clear')
+	address = input('Type in the first 6 numbers of the ip. EX 192.168  >>> ')
+	print('')
+	timeout = int(input('Type the amount of connection wait time for each host >>> '))
+	print('Scanning...')		
+	print('')
+	Fullscan(subnet = address, time = timeout)
 
-            print(f'Email Number {number} Is Being Sent')
-            
-            time.sleep(delay)
-#-------------------------------------------------------------------------------------------------------------------------------------
-            converted_num = converted_num - 1
-            if converted_num == 0:
-                os.system('sudo python RAVAGE.py')
-                break
-                
-#-------------------------------------------------------------------------------^---------------------------------------------------
-    elif ((detect == 'No') or (detect == 'no')):
-        quit()
+	choice = input('Would you like to save these to a file? y/n >>> ')
+	if(choice == 'y'):
+		print('Saving to file')
+		with open("IP_SAVED_DATA.txt", "a") as file:
+			for client in addresses:
+				file.write("IP: " + client["ip"])
+				file.write("   MAC: " + client["mac"] + '\n')
+				file.write("")
+		print('')
+		print('Done!')
+		print('')
+		input(Fore.RED+"Press Enter to continue...")
+		os.system('sudo python RAVAGE.py')
+	if(choice == 'n'):
+		input(Fore.RED+"Press Enter to continue...")
+		os.system('sudo python RAVAGE.py')
 
-    else:
-        clear_console() #Clear Console
-        print('Please Choose Option')
-        Run()
-#--------------------------------------------------------------------------------
+def Start():
+	os.system('clear')
+	print(Fore.GREEN + '██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████')
+	print('█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███')
+	print('█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███')
+	print('█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███')
+	print('█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░████░░▄▀░░███')
+	print('█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███')
+	print('█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███')
+	print('█░░░░░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░░░███')
+	print('█████████░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█████')
+	print('█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░░░░░█')
+	print('█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█')
+	print('█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█')
+	print('██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████')
+	print(Fore.WHITE+'')
+	#
+	print(Fore.CYAN + '1) Full Scan		2) Specific Scan')
+	print('')
+	selection = int(input(Fore.WHITE + 'What program is being run? >>> '))
+	#
+	if selection == 1:
+		FullScan()
+	if selection == 2:
+		SpecificScan()
+	elif (selection != 1) or (selection != 2):
+		Start()
